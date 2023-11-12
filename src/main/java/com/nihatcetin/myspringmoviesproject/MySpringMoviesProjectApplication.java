9729427368d2e7d1ec71c21eba7bd5ce52e51ca2
Nihat Cetin <nihat0851@gmail.com>
2023-11-12 16:46:17 +0300

    backend codes are done.

---
+++ src/main/java/com/nihatcetin/myspringmoviesproject/MySpringMoviesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nihatcetin.myspringmoviesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringMoviesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringMoviesProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nihatcetin/myspringmoviesproject/MySpringMoviesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nihatcetin.myspringmoviesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringMoviesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
